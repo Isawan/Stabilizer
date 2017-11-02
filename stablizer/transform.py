20e944aa1111e5fb876089e08f4f3bc97cc042a8
Isawan Millican <admin@isawan.net>
2017-11-02 00:57:56 +0000

    Changed homographic transform to Affine transformation

--- stablizer/transform.py
+++ stablizer/transform.py
@@ -5,6 +5,6 @@ import cv2
 def ransac_transform(keypoint1,keypoint2,matches):
     src = np.array([keypoint1[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
     dst = np.array([keypoint2[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
-    
-    transform_matrix,_ = cv2.findHomography(src,dst,cv2.RANSAC)
+    affine_matrix = cv2.estimateRigidTransform(src,dst,fullAffine=True)
+    transform_matrix = np.vstack([affine_matrix,[0,0,1]])
     return transform_matrix
