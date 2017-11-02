cfea33b51d81177fdd6d0c96af648c60777d12cb
Isawan Millican <admin@isawan.net>
2017-11-02 16:08:38 +0000

    Added drawer.py to help debugging

---
+++ stablizer/drawer.py
@@ -0,0 +1,11 @@
+import cv2
+
+def draw_matches(src,dst,image,color=0x00ff00):
+    assert(len(src)==len(dst))
+    for i in range(len(src)):
+        print(src[i,0])
+        cv2.arrowedLine(image,
+                tuple(src[i,0].astype(int)),
+                tuple(dst[i,0].astype(int)),
+                color,line_type=4)
+    
