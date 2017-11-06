28f53eaa58e6342fb00f73f25a2227f7a336e447
Isawan Millican <admin@isawan.net>
2017-11-06 04:40:32 +0000

    Saved memory Implemented function to find outline of trasnformed rect

--- stablizer/combine.py
+++ stablizer/combine.py
@@ -6,21 +6,19 @@ import shapely.geometry as geometry
 import stablizer.stable as stable
 import stablizer.util as util
 
-# Returns the corners rectangle after being acted on by a perspective transform
-# Define shape as (height,width)
-def find_corners(shape,tf_matrix):
-    assert(len(shape)==2)
-    s = shape
-    start_rec = np.transpose(((0,0,1),(s[0],0,1),[*s,1],(0,s[1],1)))
-    return np.transpose(np.dot(tf_matrix , start_rec)[:2])
 
 def intersect(rect1,rect2):
     a = geometry.Polygon(rect1)
     b = geometry.Polygon(rect2)
     return a.intersection(b)
 
+def transformed_rect(shape,gmatrix):
+    s = shape[1::-1] # Save typing
+    rectangle = np.array(( (0,0,1) , (0,s[1],1) , (*s,1) , (s[0],0,1) ))
+    return np.einsum('ij...,kj...->ki...',gmatrix,rectangle)[:,:2]
+
 def combine_all(video,mask,func=np.mean,erode=True):
-    final = np.zeros(video.shape[1:])
+    final = np.zeros(video.shape[1:],np.uint8)
     if erode:
         kernel = np.ones((3,3),np.uint8)
         for m in range(mask.shape[0]):
@@ -36,14 +34,13 @@ def combine_all(video,mask,func=np.mean,erode=True):
 
 
 if __name__ == '__main__':
-    video = util.loadfile('resources/sample2.mp4')
+    video = util.loadfile('resources/measure.mp4')[:,::2,::2]
     print(video.shape)
     print('Video loaded')
     stablized_video,info = stable.stablize_video(video,extra=True)
-    print(info['localmt'][10])
     print('Video stablized')
     print(info['mask'].dtype)
-    final = combine_all(stablized_video,info['mask'],np.mean)#lambda x: x[-1])
+    final = combine_all(stablized_video,info['mask'],np.mean)#,lambda x: x[-1])
     print(final.shape)
     plt.imshow(final,'Greys_r')
     plt.show()
--- stablizer/match.py
+++ stablizer/match.py
@@ -3,12 +3,22 @@ import cv2
 
 # Returns a dict that maps keypoint1 -> keypoint2
 # Not all keypoints are garunteed to be mapped
-def match(keypoint1,descriptor1,keypoint2,descriptor2):
+def match(keypoint1,descriptor1,keypoint2,descriptor2,maxdist=300):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     matches = bf.knnMatch(descriptor1,descriptor2,k=2)
     good = []
+    prox = []
+    # Apply ratio test
     for m,n in matches:
         if m.distance < 0.8*n.distance:
             good.append(m)
-    return sorted(good,key=lambda x:x.distance)
+
+    # Proximity test
+    for m in good:
+        dist = np.linalg.norm(np.array(keypoint1[m.queryIdx].pt)-
+                np.array(keypoint2[m.trainIdx].pt))
+        if dist < maxdist:
+            prox.append(m)
+
+    return sorted(prox,key=lambda x:x.distance)
 
