745cafd1d1f337316066397377dfef12115a258e
Isawan Millican <admin@isawan.net>
2017-11-06 04:42:01 +0000

    Removed print statement

--- stablizer/drawer.py
+++ stablizer/drawer.py
@@ -3,7 +3,6 @@ import cv2
 def draw_matches(src,dst,image,color=0x00ff00):
     assert(len(src)==len(dst))
     for i in range(len(src)):
-        print(src[i,0])
         cv2.arrowedLine(image,
                 tuple(src[i,0].astype(int)),
                 tuple(dst[i,0].astype(int)),
